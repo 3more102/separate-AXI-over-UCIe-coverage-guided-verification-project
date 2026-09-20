2b1beee7744a31ed7537e0d367bb13603c8e2512
omar islam <s-omar.abdalla@zewailcity.edu.eg>
2026-09-20 19:35:18 +0300

    build: add Questa compilation file list

---
+++ sim/files.f
@@ -0,0 +1,7 @@
+../tb/interfaces/axi_if.sv
+../tb/interfaces/ucie_link_if.sv
+../rtl/axi_ucie_bridge.sv
+../tb/ucie_responder_model.sv
+../tb/axi_ucie_pkg.sv
+../tb/assertions/axi_protocol_sva.sv
+../tb/top.sv
